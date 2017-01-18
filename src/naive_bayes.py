5f001f8b98bb0dfe504fb1011921860354de2afb
Yogesh Yadav <yogesh.yadav@MXMCR30A3DTY3.local>
2017-01-18 13:31:06 -0600

    First Commit Project Structure

---
+++ src/naive_bayes.py
@@ -0,0 +1,10 @@
+"""
+Naive Bayes Classification
+Supervised Learning - Classification technique
+What is does and how - https://www.youtube.com/watch?v=EGKeC2S44Rs
+Maths behind - Very Simple Probability - http://scikit-learn.org/stable/modules/naive_bayes.html
+
+"""
+
+
+
